1263e060c027ef88e822b7317674c1d80df0e57f
Giovanni <glicameli@outlook.com>
2020-04-08 23:11:22 -0500

    Corrected Newline interpreter Error

--- partition-resize.sh
+++ partition-resize.sh
@@ -9,19 +9,19 @@ echo 'Using /dev/$DRIVE'
 
 MAIN_DRIVE = "/dev/$DRIVE"
 # Assumes Gigabytes
-declare -i TOTAL_SPACE=$(fdisk -l | grep $DRIVE | cut -d':' -f2 | cut -d',' -f1 | cut -d' ' -f2 | cut -d#'\n' -f1)
+declare -i TOTAL_SPACE=$(fdisk -l | grep $DRIVE | cut -d':' -f2 | cut -d',' -f1 | cut -d' ' -f2 | awk 'FNR == 1 {print}')
 STORAGE=$((TOTAL_SPACE - 4))
 # Save 2 GB just in case. No need really.
 SWAP=2
 sed -i 's/+G/+'$STORAGE'G/g' import.txt
 sed -i 's/++G/+'$SWAP'G/g' import.txt
 
-cat import | fdisk $MAIN_DRIVE
+cat import.txt | fdisk $MAIN_DRIVE
 
 partprobe
 resize2fs $MAIN_DRIVE"1"
 mkswap $MAIN_DRIVE"2"
 SWAP_UUID=$(blkid | grep "swap" | awk '/UUID="*"/ {print $2}' | cut -d'"' -f2)
-ORIG_UUID=$(cat /etc/fstab | grep swap | cut -d'=' -f2 | cut -d' ' -f1 | cut -d$'\n' -f2)
+ORIG_UUID=$(cat /etc/fstab | grep swap | cut -d'=' -f2 | cut -d' ' -f1 | awk 'FNR == 2 {print}')
 sed -i '/^.*swap*/s/UUID='$ORIG_UUID'/UUID='$SWAP_UUID'/g' /etc/fstab
 swapon -a
